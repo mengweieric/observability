b3dbdcf8aa6b2a087fca24ad9b598631223297a4
Eric Wei <menwe@amazon.com>
2021-12-09 10:33:12 -0800

    added dir

--- dashboards-observability/.eslintrc.js
+++ dashboards-observability/.eslintrc.js
@@ -7,8 +7,13 @@ module.exports = {
   root: true,
   extends: [
     '@elastic/eslint-config-kibana',
-    'plugin:@elastic/eui/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    'plugin:@elastic/eui/recommended'
   ],
+  "overrides": [
+    {
+      "files": ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      "excludedFiles": "*.test.js",
+    }
+  ]
 };
-
